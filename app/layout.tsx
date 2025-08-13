0b0bdb9cc4bc6db01ff26a8668ab1f022df9f316
Aidan Shotland <aidan.shotland@gmail.com>
2025-08-13 14:44:37 -0700

    Added Check Auth State

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-
 import type { Metadata } from "next";
 import "./globals.css";
 import NavMenu from "./NavMenu";
-import { Open_Sans } from 'next/font/google';
+import { Open_Sans } from "next/font/google";
+import AuthProvider from "./AuthProvider";
 
-const myFont = Open_Sans({ weight: '400', subsets: ['latin'] });
+const myFont = Open_Sans({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={myFont.className}>
-        <NavMenu />
-        {children}
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={myFont.className}>
+          <NavMenu />
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
   );
 }

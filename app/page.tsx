c31f8dd7ce66178fdfaae4505ff7b1a7d10eafae
Aidan Shotland <aidan.shotland@gmail.com>
2025-08-21 11:52:20 -0700

    Added server component fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import styles from "./page.module.css";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    redirect("/api/auth/signin");
-  }
-
-  return <div></div>;
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to NextSpace!</h1>
+      <p>
+        A next-gen social media app to connect with frens inspired by MySpace
+      </p>
+      <p>To get started, sign up for an account</p>
+    </div>
+  );
 }
